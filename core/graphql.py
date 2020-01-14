4a1b2392b92e68fe0a241cf316f40d7ed098fbc6
Robert Roskam <raiderrobert@gmail.com>
2020-01-14 16:33:53 -0500

    Made relationships and stuff

--- core/graphql.py
+++ core/graphql.py
@@ -2,12 +2,23 @@ import graphene
 from graphene_pydantic import PydanticObjectType
 
 from typing import Union
-from core.models import CivilizationsModel, get_civs
+from core.models import CivilizationsModel, get_civs, UnitsModel, get_units, get_unit
 
 
 class Civilization(PydanticObjectType):
+
+    uniqueUnit = graphene.List(lambda: Unit)
+
     class Meta:
         model = CivilizationsModel
+
+    def resolve_uniqueUnit(self, info):
+        return [get_unit(f) for f in self.uniqueUnit]
+
+
+class Unit(PydanticObjectType):
+    class Meta:
+        model = UnitsModel
 
 
 class Query(graphene.ObjectType):
@@ -17,6 +28,7 @@ class Query(graphene.ObjectType):
         name=graphene.Argument(type=graphene.String, required=False),
         speciality_contains=graphene.Argument(type=graphene.String, required=False),
     )
+    units = graphene.List(Unit,)
 
     def resolve_civilizations(
         self,
@@ -29,12 +41,17 @@ class Query(graphene.ObjectType):
     ):
         civs = get_civs()
         if version:
-            civs = (x for x in civs if x.__getattribute__('ver') == version)
+            civs = (x for x in civs if x.__getattribute__("ver") == version)
         if name:
-            civs = (x for x in civs if x.__getattribute__('name') == name)
+            civs = (x for x in civs if x.__getattribute__("name") == name)
         if speciality_contains:
-            civs = (x for x in civs if speciality_contains.lower() in x.speciality.lower())
+            civs = (
+                x for x in civs if speciality_contains.lower() in x.speciality.lower()
+            )
         return civs
+
+    def resolve_units(self, info, *args, **kwargs):
+        return get_units()
 
 
 schema = graphene.Schema(query=Query)
--- core/models.py
+++ core/models.py
@@ -4,6 +4,16 @@ import typing
 import pathlib
 
 
+civ_data = {}
+unit_data = {}
+
+
+def get_data(file_name) -> str:
+    file = pathlib.Path(__file__).resolve().parent.parent.joinpath(f"data/{file_name}")
+    with open(file, "r") as reader:
+        return reader.read()
+
+
 class CivilizationsModel(BaseModel):
     name: str
     version: str
@@ -18,19 +28,37 @@ class CivilizationsModel(BaseModel):
     teamBonus: str
 
 
-def get_data(file_name) -> str:
-    file = pathlib.Path(__file__).resolve().parent.parent.joinpath(f"data/{file_name}")
-    with open(file, "r") as reader:
-        return reader.read()
-
-
-def get_civilization_data() -> str:
-    return get_data('civilizations.json')
-
-
-def get_unit_data() -> str:
-    return get_data('units.json')
+def get_civ(name):
+    return civ_data.get(name)
 
 
 def get_civs() -> typing.List[CivilizationsModel]:
-    return [CivilizationsModel(**x) for x in json.loads(get_civilization_data())]
+    return [model for model in civ_data.values()]
+
+
+class UnitsModel(BaseModel):
+    name: str
+    type: str
+
+
+def get_unit(name):
+    return unit_data.get(name)
+
+
+def get_units() -> typing.List[UnitsModel]:
+    return [model for model in unit_data.values()]
+
+
+def setup_data():
+    global civ_data, unit_data
+
+    civ_data = {
+        x['name']: CivilizationsModel(**x) for x in json.loads(get_data("civilizations.json"))
+    }
+
+    unit_data = {
+        x['name']: UnitsModel(**x) for x in json.loads(get_data("units.json"))
+    }
+
+
+setup_data()
